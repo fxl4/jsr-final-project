3a8f9085a7daa5d11cfc1285099fe438b9cba00e
fxl4 <fxlarkin4@gmail.com>
2023-07-24 11:59:23 -0400

    first pass at final project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const router = express.Router();
+
+const apiKey = "0348c3f2d4ae4057b8a171057230407";
+let inputZipCode;
+let outputCity;
+let outputTemperature;
+
+// Element selectors
+
+// Event handlers
+
+// Event listeners
